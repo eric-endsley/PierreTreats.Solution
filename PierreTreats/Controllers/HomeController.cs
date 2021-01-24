9af60dd31f51084a43b7cef5d6bb6e34eeec8e00
Eric Endsley <eric.endsley4@gmail.com>
2021-01-24 14:49:18 -0800

    add splash page, modify home controller to pass data to viewbag

--- PierreTreats/Controllers/HomeController.cs
+++ PierreTreats/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using PierreTreats.Models;
 
 namespace PieereTreats.Controllers
 {
   public class HomeController : Controller
   {
-    [HttpGet("/")]
-    public ActionResult Index()
-    {
-      return View();
-    }
+      private readonly PierreTreatsContext _db;
+
+      public HomeController(PierreTreatsContext db)
+      {
+          _db = db;
+      }
+
+      public ActionResult Index()
+      {
+          ViewBag.Treats = _db.Treats;
+          ViewBag.Flavors = _db.Flavors;
+          return View();
+      }
   }
 }
