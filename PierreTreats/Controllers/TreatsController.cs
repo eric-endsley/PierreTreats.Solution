7171460022cebd9d9214088757206c076c742e75
Eric Endsley <eric.endsley4@gmail.com>
2021-01-24 16:48:11 -0800

    add all anticipated routes to treats controller

---
+++ PierreTreats/Controllers/TreatsController.cs
@@ -0,0 +1,105 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using PierreTreats.Models;
+
+namespace PierreTreats.Controllers
+{
+    public class TreatsController : Controller
+    {
+        private readonly PierreTreatsContext _db;
+
+        public TreatsController(PierreTreatsContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Treat> treatList = _db.Treats.ToList()
+            return View(treastList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Treat treat)
+        {
+            _db.Treats.Add(treat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+          
+        }
+
+        public ActionResult Details(int id)
+        {
+            var thisTreat = _db.Treats
+                .Include(treat => treat.Flavors)
+                .ThenInclude(join => join.Treat)
+                .FirstOrDefault(treat => treat.TreatId)
+            return View(thisTreat);
+        }
+
+        public ActionResult AddFlavor(int id)
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treat => flavor.TreatId == id);
+            ViewBag.TreatId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+            return View(thisTreat);
+        }
+
+        [HttpPost]
+        public ACtionResult AddFlavor(Treat treat, int FlavorId)
+        {
+            if (FlavorId != 0)
+            {
+                var returnedJoin = _db.TreatFlavor
+                    .Any(join => join.FlavorId == FlavorId && join.TreatId == treat.TreatId);
+                if (!returnedJoin)
+                {
+                    _db.TreatFlavor.Add(new TreatFlavor () { TreatId = treat.TreatId, FlavorId = FlavorId });
+                }
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Details", new { id = treat.TreatId });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treat => flavor.TreatId == id);
+            ViewBag.TreatId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+            return View(thisTreat);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Treat treat, int FlavorId)
+        {
+            if (FlavorId != 0)
+            {
+                _db.TreatFlavor.Add(new TreatFlavor () { TreatId = treat.TreatId, FlavorId = FlavorId });
+            }
+            _db.Entry(treat).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisTreat = _db.Treatss.FirstOrDefault(treat => treat.TreatId == id);
+            _db.Treats.Remove(thisTreat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
